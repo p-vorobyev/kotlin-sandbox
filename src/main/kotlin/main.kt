d277a124616c73e647e845f27d145ba407927896
Pavel Vorobev <p.vorobyev@inbox.ru>
2021-09-20 00:48:44 +0300

    data classes & enums

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,19 @@
+open class Lion(private val name: String,
+                private val origin: String) {
+    fun sayHello() {
+        println("$name, the lion from $origin says: graoh!")
+    }
+}
+
+class Asiatic(name: String) : Lion(name = name, origin = "India")
+
+val authors = setOf("Shakespeare", "Hemingway", "Twain")
+val writers = setOf("Twain", "Shakespeare", "Hemingway")
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val lion: Lion = Asiatic("Rufo")
+    lion.sayHello()
+
+    println(authors == writers)
+    println(authors === writers)
 }
