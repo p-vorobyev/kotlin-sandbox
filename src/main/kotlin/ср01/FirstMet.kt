9afbae5774850450bde5b7b94e9bbcf58752629e
Воробьев Павел Сергеевич <PSVorobyev@sberbank.ru>
2021-10-08 10:16:53 +0300

    first met

---
+++ src/main/kotlin/ср01/FirstMet.kt
@@ -0,0 +1,14 @@
+package ср01
+
+data class Person(val name: String,
+                  val age: Int? = null)
+
+fun maxInt(a: Int, b: Int): Int = if (a > b) a else b
+
+fun main() {
+    val persons = listOf(Person("Alice"), Person("Bob", 29))
+    val oldest = persons.maxByOrNull { it.age ?: 0 }
+    if (oldest is Person) {
+        println("The oldest is: $oldest")
+    }
+}
